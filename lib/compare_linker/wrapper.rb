0c16daf5bfea44021d1612b2740f665b6781c387
sanemat <o.gata.ken@gmail.com>
2015-03-12 21:18:56 +0900

    Initial commit

---
+++ lib/compare_linker/wrapper.rb
@@ -0,0 +1,7 @@
+require "compare_linker/wrapper/version"
+
+module CompareLinker
+  module Wrapper
+    # Your code goes here...
+  end
+end
---
+++ lib/compare_linker/wrapper/version.rb
@@ -0,0 +1,5 @@
+module CompareLinker
+  module Wrapper
+    VERSION = "0.1.0"
+  end
+end
