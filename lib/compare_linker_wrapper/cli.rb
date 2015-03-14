c76fcad0259dd9fa0256957ddd780cd33828985b
sanemat <o.gata.ken@gmail.com>
2015-03-15 02:22:50 +0900

    chore(compare): implement compare

--- lib/compare_linker_wrapper/cli.rb
+++ lib/compare_linker_wrapper/cli.rb
@@ -19,50 +19,53 @@ module CompareLinkerWrapper
     option :base, type: :string, default: 'origin/master'
     option :head, type: :string, default: 'HEAD'
 
-    def compare(*_args)
+    def compare(*args)
       setup_logger(options)
-      gemfile_locks = [
-        'Gemfile.lock'
-      ]
+      gemfile_locks = args[1..-1]
       params = {
-        base: options[:base],
+        base: options[:base] || args[0],
         head: options[:head]
       }
+      puts gemfile_locks
+      puts params
       access_token = ENV['OCTOKIT_ACCESS_TOKEN'] || ENV['GITHUB_ACCESS_TOKEN']
-      octokit ||= Octokit::Client.new(access_token: access_token)
+      octokit ||= ::Octokit::Client.new(access_token: access_token)
       formatter = ::CompareLinker::Formatter::Text.new
       git = Git.open('.')
-      old_lockfile = parse(git.show(params[:base], gemfile_locks[0]))
-      new_lockfile = parse(git.show(params[:head], gemfile_locks[0]))
+      gemfile_locks.each do |gemfile_lock|
+        old_lockfile = parse(git.show(params[:base], gemfile_lock))
+        new_lockfile = parse(git.show(params[:head], gemfile_lock))
 
-      comparator = ::CompareLinker::LockfileComparator.new
-      comparator.compare(old_lockfile, new_lockfile)
-      compare_links = comparator.updated_gems.map do |gem_name, gem_info|
-        if gem_info[:owner].nil?
-          finder = ::CompareLinker::GithubLinkFinder.new(octokit)
-          finder.find(gem_name)
-          if finder.repo_owner.nil?
-            gem_info[:homepage_uri] = finder.homepage_uri
-            formatter.format(gem_info)
-          else
-            gem_info[:repo_owner] = finder.repo_owner
-            gem_info[:repo_name] = finder.repo_name
-
-            tag_finder = ::CompareLinker::GithubTagFinder.new(octokit)
-            old_tag = tag_finder.find(finder.repo_full_name, gem_info[:old_ver])
-            new_tag = tag_finder.find(finder.repo_full_name, gem_info[:new_ver])
-
-            if old_tag && new_tag
-              gem_info[:old_tag] = old_tag.name
-              gem_info[:new_tag] = new_tag.name
+        comparator = ::CompareLinker::LockfileComparator.new
+        comparator.compare(old_lockfile, new_lockfile)
+        compare_links = comparator.updated_gems.map do |gem_name, gem_info|
+          if gem_info[:owner].nil?
+            finder = ::CompareLinker::GithubLinkFinder.new(octokit)
+            finder.find(gem_name)
+            if finder.repo_owner.nil?
+              gem_info[:homepage_uri] = finder.homepage_uri
               formatter.format(gem_info)
             else
-              formatter.format(gem_info)
+              gem_info[:repo_owner] = finder.repo_owner
+              gem_info[:repo_name] = finder.repo_name
+
+              tag_finder = ::CompareLinker::GithubTagFinder.new(octokit)
+              old_tag = tag_finder.find(finder.repo_full_name, gem_info[:old_ver])
+              new_tag = tag_finder.find(finder.repo_full_name, gem_info[:new_ver])
+
+              if old_tag && new_tag
+                gem_info[:old_tag] = old_tag.name
+                gem_info[:new_tag] = new_tag.name
+                formatter.format(gem_info)
+              else
+                formatter.format(gem_info)
+              end
             end
+          else
+            formatter.format(gem_info)
           end
-        else
-          formatter.format(gem_info)
         end
+        puts compare_links
       end
     rescue StandardError => e
       suggest_messages(options)
